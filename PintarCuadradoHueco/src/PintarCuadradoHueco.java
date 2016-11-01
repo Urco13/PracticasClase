68d8bc0cbfabcfd0a42312e9abf655bef1fa212a
Urco <>
2016-11-01 19:37:43 +0100

    Programa Cuadrado hueco resuelto

--- PintarCuadradoHueco/src/PintarCuadradoHueco.java
+++ PintarCuadradoHueco/src/PintarCuadradoHueco.java
@@ -11,15 +11,30 @@
 import java.util.Scanner;
 public class PintarCuadradoHueco {
     public static void main(String[]args){
-        int i, lado;
+        int i, a, b, lado;
         Scanner teclado = new Scanner(System.in);
         System.out.println("Dime el un numero");
         lado = teclado.nextInt();
+        
         for(i=1; i<=lado; i++){
            if(i==1 || i==lado){
-               for(int a=)
-           } 
-        }
+               //pintamos el primer y ultimo lado
+               for(a=1; a<=lado; a++){
+                System.out.print("* ");
+                }
+               System.out.println("");
+           }else{
+                if(i>1 && i<lado){
+                  for(b=1; b<=lado; b++){
+                      if(b==1 || b==lado){
+                          System.out.print("* ");
+                      }else{
+                          System.out.print("  ");
+                      }                    
+                  }
+                   System.out.println("");
+                }           
+            }
+        }   
     }
-            
 }
