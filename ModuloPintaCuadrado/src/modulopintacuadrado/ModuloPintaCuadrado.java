da171117136d3c35e8055e31f8e6e16f3e7bf3e7
dam115 <dam115@PC2015.aulas.local>
2016-11-07 14:40:28 +0100

    1.0

---
+++ ModuloPintaCuadrado/src/modulopintacuadrado/ModuloPintaCuadrado.java
@@ -0,0 +1,19 @@
+/**
+ * Pinta Cuadrado
+ * Crear modulo para pintar cuadrado
+ * recibe el valor del lado y devuelve el area del cuadrado
+ */
+package modulopintacuadrado;
+
+/**
+ *
+ * @author dam115
+ */
+public class ModuloPintaCuadrado {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
