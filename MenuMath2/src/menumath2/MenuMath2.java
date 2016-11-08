afa60970f804c89614270cf8a800e5a168ac2c1e
dam115 <dam115@PC2015.aulas.local>
2016-11-08 10:53:39 +0100

    1.0

---
+++ MenuMath2/src/menumath2/MenuMath2.java
@@ -0,0 +1,24 @@
+/**
+ * Implementar dos metodos de calculos de distinta forma para los
+ * apartados anteriores
+ * Leer datos y mostrar el resultado del calculo
+ * leer datos y devuelve el resultado
+ * D)Implementar el tercer metodo de manera que pasamos datos
+ * como argumentos y devuelve el resultado
+ */
+package menumath2;
+
+/**
+ *
+ * @author dam115
+ */
+public class MenuMath2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
