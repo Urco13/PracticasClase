5cabf598f4470d12272e7171092b29a028a5b57c
Urco13 <urco13@gmail.com>
2016-11-14 14:41:40 +0100

    Add files via upload

---
+++ AdivinarNumero/src/adivinarnumero/AdivinarNumero.java
@@ -0,0 +1,18 @@
+/**
+ * El programa indicara al jugador si esta muy cerca o lejos segun la distancia
+ * al numero a adivinar
+ * Se jugara mientras no acierte o tenga intentos
+ * Dos Jugadores. Un jugador introduce un numero entre 1 y 100 y el otro 
+ * jugador tiene 5 intentos para adivinarlo
+ */
+package adivinarnumero;
+
+public class AdivinarNumero {
+
+    
+    public static void main(String[] args) {
+        
+    }//fin main
+    
+    
+}
