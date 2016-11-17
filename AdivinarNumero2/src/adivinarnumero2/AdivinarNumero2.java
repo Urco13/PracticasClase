154d7d0fcd4f13158b8e5b301996c6993439dac0
Urco13 <urco13@gmail.com>
2016-11-17 09:46:18 +0100

    Add files via upload

--- AdivinarNumero2/src/adivinarnumero2/AdivinarNumero2.java
+++ AdivinarNumero2/src/adivinarnumero2/AdivinarNumero2.java
@@ -18,7 +18,7 @@ public class AdivinarNumero2 {
     
     public static void main(String[] args) throws IOException {
         String cadena = menuPrograma();
-        
+        empezarJuego(cadena);
     }//fin main
     
     //metodo numero aleatorio
@@ -27,23 +27,53 @@ public class AdivinarNumero2 {
         int numeroAleatorio = r.nextInt(100)+1;
         return numeroAleatorio;
     }
-    //Leer teclado string devolver string
-    public static String leerTeclado(String texto) throws IOException{
+    //Leer string devolver string
+    public static String leerTecladoString(String texto) throws IOException{
         
         System.out.println(texto);
         String numeroTeclado = teclado.readLine();
         return numeroTeclado;
     }
+    //Leer string devolver int
+    public static int leerTecladoInt(String texto) throws IOException{
+        
+        System.out.println(texto);
+        int numeroTeclado = Integer.parseInt(teclado.readLine());
+        return numeroTeclado;
+    }
     //Menu del programa
     public static String menuPrograma() throws IOException{
         
-        String siNo =leerTeclado("Este juego es para ver si adivinas un numero \n"
+        String siNo =leerTecladoString("Este juego es para ver si adivinas un numero \n"
                 + "Quieres comenzar S/N");
         return siNo;
     }
     //Comprobacion
-    public static void empezarJuego(String siNO){
-       
+    public static void empezarJuego(String cadena) throws IOException{
+        int numero=numeroAleatorio();
+       if("S".equals(cadena)){
+           for(int i = 1; i<=5; i++){
+                int resultado =leerTecladoInt("Este es tu " + i + " introduce el numero");
+                if(resultado == numero){
+                    System.out.println("Eres un fenomeno acertastes en tu " + i + " intento");
+                    return;
+                }else if(resultado > numero){
+                    if(Math.abs(resultado - numero) < 10){
+                        System.out.println("Te has pasado pero estas muy cerca");
+                    }else if(Math.abs(resultado - numero) < 30){
+                        System.out.println("Te has pasado pero estas cerca");
+                    }else 
+                          System.out.println("Te has pasado estas muy lejos");
+                }else if(resultado < numero){
+                    if(Math.abs(resultado - numero) < 10){
+                        System.out.println("Te has quedado corto pero estas muy cerca");
+                    }else if(Math.abs(resultado - numero) < 30){
+                        System.out.println("Te has quedado corto pero estas cerca");
+                    }else 
+                          System.out.println("Te has que dado corto estas muy lejos");
+                }
+            }
+        }else
+            System.out.println("Nos vemos");
     }
-    
 }//fin class AdivinarNumero2
