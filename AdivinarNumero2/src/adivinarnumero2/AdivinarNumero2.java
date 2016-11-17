ed3889fae4f6fdf5da68741c4b0116dd533e70ea
Urco13 <urco13@gmail.com>
2016-11-17 18:23:59 +0100

    Add files via upload

--- AdivinarNumero2/src/adivinarnumero2/AdivinarNumero2.java
+++ AdivinarNumero2/src/adivinarnumero2/AdivinarNumero2.java
@@ -6,54 +6,83 @@
 package adivinarnumero2;
 
 /**
- * @author dam115
+ *
+ * @author urco1
  */
 import java.util.Random;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-public class AdivinarNumero2 {
 
+
+public class AdivinarNumero2 {
+ 
     static BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-    
+ 
     public static void main(String[] args) throws IOException {
-        String cadena = menuPrograma();
+         String cadena = menuPrograma();
         empezarJuego(cadena);
     }//fin main
     
-    //metodo numero aleatorio
+    /**metodo numero aleatorio
+     * 
+     * @return 
+     */
     public static int numeroAleatorio(){
         Random r = new Random();
         int numeroAleatorio = r.nextInt(100)+1;
         return numeroAleatorio;
     }
-    //Leer string devolver string
+    
+    /**Leer string devolver string
+     * 
+     * @param texto
+     * @return
+     * @throws IOException 
+     */
     public static String leerTecladoString(String texto) throws IOException{
         
         System.out.println(texto);
         String numeroTeclado = teclado.readLine();
         return numeroTeclado;
     }
-    //Leer string devolver int
+    
+    /**Leer string devolver int
+     * 
+     * @param texto
+     * @return
+     * @throws IOException 
+     */
+    
     public static int leerTecladoInt(String texto) throws IOException{
         
         System.out.println(texto);
         int numeroTeclado = Integer.parseInt(teclado.readLine());
         return numeroTeclado;
     }
-    //Menu del programa
+    
+    /**Menu del programa
+     * 
+     * @return
+     * @throws IOException 
+     */
     public static String menuPrograma() throws IOException{
         
         String siNo =leerTecladoString("Este juego es para ver si adivinas un numero \n"
                 + "Quieres comenzar S/N");
         return siNo;
     }
-    //Comprobacion
+    
+    /**Juego
+     * 
+     * @param cadena
+     * @throws IOException 
+     */
     public static void empezarJuego(String cadena) throws IOException{
         int numero=numeroAleatorio();
-       if("S".equals(cadena)){
+       if("S".equals(cadena) || "s".equals(cadena)){
            for(int i = 1; i<=5; i++){
-                int resultado =leerTecladoInt("Este es tu " + i + " introduce el numero");
+                int resultado =leerTecladoInt("Este es tu " + i + " intento introduce el numero");
                 if(resultado == numero){
                     System.out.println("Eres un fenomeno acertastes en tu " + i + " intento");
                     return;
@@ -76,4 +105,4 @@ public class AdivinarNumero2 {
         }else
             System.out.println("Nos vemos");
     }
-}//fin class AdivinarNumero2
+}// fin class
