7e56b9a05d5c05bdac4814c82b4171e8bc60f5d5
Urco13 <urco13@gmail.com>
2016-11-22 10:23:59 +0100

    Add files via upload

--- PintarCuadrado/src/PintarCuadrado.java
+++ PintarCuadrado/src/PintarCuadrado.java
@@ -8,11 +8,16 @@
  *
  * @author dam115
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class PintarCuadrado {
     public static void main(String[]args){
         int lado, i;
         Scanner teclado = new Scanner(System.in);
+        //Scanner teclado = null; new Scanner(System.in) Se define el objeto
+        //pero se deja en null; Si esta definido el ojeto como null nos saltarina
+        //la exception NullPointerException
+        try{
         System.out.println("Dime el numero de lados de tu cuadrado");
         lado = teclado.nextInt();
         for(int a=1; a<=lado; a++){
@@ -20,6 +25,19 @@ public class PintarCuadrado {
                 System.out.printf("* ");
         }
             System.out.print("\n");
-        }  
+        } 
+        }catch(InputMismatchException objeto){//Esta exception salta con dos errores
+            //numero decimal con punto y numero mas caracter
+            System.out.println(objeto.getCause());
+            System.out.println(objeto.getClass());
+            System.out.println("Esto es otra prueba");
+            System.out.println(objeto.toString());
+            objeto.printStackTrace();
+        }catch(NullPointerException objeto){
+            System.out.println("Objeto no existe: "+ objeto.getMessage());
+        }catch(Exception objeto){
+            System.out.println(objeto.getCause());
+           
+        }
     }
 }
