4cd16fda3f115f475cd776c2e8e86a4e3df2bd57
Carlos <urco13@gmail.com>
2016-12-09 10:54:32 +0100

    Add files via upload

---
+++ Arrays2/src/arrays2/Arrays2.java
@@ -0,0 +1,42 @@
+/**
+ * Llenamos un string con 4 elementos
+ * System.arraycopy()
+ * Arrays.binarySearch():buscar valor en orde
+ * Array.fill()
+ * Array.equals()
+ * Arrays.copyOf()
+ * Arrays.copyOfRange()
+ */
+package arrays2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ *
+ * @author dam115
+ */
+public class Arrays2 {
+    BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String matriz[]={"Juan","Pedro","Antonio","Maria","Jaina"};
+        Arrays.sort(matriz);
+        for(int i = 0; i<matriz.length; i++){
+            System.out.println(matriz[i]);
+        }
+        System.out.println("Este es binarySearch");
+            //Siempre hay que ordenar el array antes de usar binarysearch
+         int posicion= Arrays.binarySearch(matriz,"Maria");
+         //if (Arrays.binarySearch
+         System.out.println(posicion);
+         
+        System.out.println("Este es fill");
+        Arrays.fil
+         
+    }//fin main
+  
+}//fin class
